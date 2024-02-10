34e463c65b7e92778fd532eebc56f01259006232
Dan Mwamba <danmuamba81@gmail.com>
2024-02-10 01:19:30 +0100

    feat: configure the path to the users profile page and the addition of a cover on the tweet-profile page

--- src/components/trends/follower.jsx
+++ src/components/trends/follower.jsx
@@ -5,7 +5,7 @@ import { vector } from "../../icons";
 function Follower({ user, title, srcImg, profileVerified,tweet }) {
   return (
     <div className="account-follow">
-      <Avatar source={tweet.userAvatar} />
+      <Avatar source={tweet.avatar} />
       <InfoUser name={tweet.userName} title={title} userVerified={vector}/>
       <ButtonFollow style={"btn-follow"} />
     </div>
--- src/pages/tweet-profile.jsx
+++ src/pages/tweet-profile.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useTweets } from "../context/TweetContext";
-import { Tweet } from "../components";
+import { Tweet, Avatar } from "../components";
 
 
 function ProfileTweet() {
@@ -9,14 +9,28 @@ function ProfileTweet() {
    
   let {tweets} = useTweets()
 
-  const userTweets = tweets.filter(tweet => tweet.userName === userName)
+  const userTweets = tweets.find(tweet => tweet.userName === userName);
+  const listUserTweet = tweets.filter(user => user.userName === userTweets.userName)
   
 
    return(
     <>
      <main className="timeline">
-         
-      {userTweets.map((tweet) => (
+      
+      <div className="h-32 overflow-hidden">
+        <img className="w-full" src={userTweets.imageTweet}/>
+      </div>
+
+      <div className="px-5 -mt-12">
+        <Avatar source={userTweets.avatar} className="h-32 w-32 bg-white p-2"/> 
+      </div>
+      <div className="m-5">
+        <p className="font-bold text-lg">{userTweets.userName}</p>
+        <p className="text-gray-400">{userTweets.infoTweet}</p>
+      </div>
+      
+     
+      {listUserTweet.map((tweet) => (
         <Tweet key={tweet.id} tweet={tweet}/>
       ))}
     
